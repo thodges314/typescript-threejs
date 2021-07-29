6577a255106c7e3f7de9a36aaa751ba3432e7bee
Thomas Hodges <thodges@javascriptonline.org>
2021-07-28 20:11:37 -0700

    lots of scenes

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,13 +2,20 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
+const scene2 = new THREE.Scene();
 
 scene.background = new THREE.Color(0x000000);
 
 const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
-const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
-camera1.position.z = 2;
-camera2.position.z = 2;
+const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1);
+const camera3 = new THREE.OrthographicCamera(-1, 1, 1, -1);
+const camera4 = new THREE.OrthographicCamera(-1, 1, 1, -1);
+camera1.position.z = 2; // from front
+camera2.position.y = 2;
+camera2.lookAt(new THREE.Vector3()); // default is origin
+camera3.position.x = -2;
+camera3.lookAt(new THREE.Vector3(0, 0, 0));
+camera4.position.z = 2;
 
 const canvas1 = document.getElementById("c1") as HTMLCanvasElement;
 const canvas2 = document.getElementById("c2") as HTMLCanvasElement;
@@ -26,29 +33,26 @@ renderer2.setSize(200, 200);
 
 new OrbitControls(camera1, renderer1.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.TorusKnotGeometry(); //THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
   wireframe: true,
 });
 
 const cube = new THREE.Mesh(geometry, material);
+cube.scale.x = 0.5;
+cube.scale.y = 0.5;
+cube.scale.z = 0.5;
 scene.add(cube);
 
-console.dir(scene);
-
-// window.addEventListener("resize", onWindowResize, false);
-// function onWindowResize() {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer2.setSize(window.innerWidth, window.innerHeight);
-//   render();
-// }
+const cube2 = new THREE.Mesh(geometry, material);
+scene2.add(cube2);
 
 function animate() {
   requestAnimationFrame(animate);
 
   cube.rotation.x += 0.01;
+  cube2.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
   render();
@@ -56,9 +60,9 @@ function animate() {
 
 function render() {
   renderer1.render(scene, camera1);
-  renderer2.render(scene, camera2);
-  renderer3.render(scene, camera1);
-  renderer4.render(scene, camera1);
+  renderer2.render(scene2, camera2);
+  renderer3.render(scene, camera3);
+  renderer4.render(scene, camera4);
 }
 
 animate();

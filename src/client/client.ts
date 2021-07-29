7c52a668c2be76fc324d563d2c1707687f9face2
Thomas Hodges <thodges@javascriptonline.org>
2021-07-28 20:01:29 -0700

    more cameras

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,19 +3,28 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 2;
+scene.background = new THREE.Color(0x000000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
+const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
+camera1.position.z = 2;
+camera2.position.z = 2;
 
-new OrbitControls(camera, renderer.domElement);
+const canvas1 = document.getElementById("c1") as HTMLCanvasElement;
+const canvas2 = document.getElementById("c2") as HTMLCanvasElement;
+const canvas3 = document.getElementById("c3") as HTMLCanvasElement;
+const canvas4 = document.getElementById("c4") as HTMLCanvasElement;
+
+const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
+renderer1.setSize(200, 200);
+const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
+renderer2.setSize(200, 200);
+const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3 });
+renderer2.setSize(200, 200);
+const renderer4 = new THREE.WebGLRenderer({ canvas: canvas4 });
+renderer2.setSize(200, 200);
+
+new OrbitControls(camera1, renderer1.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -28,13 +37,13 @@ scene.add(cube);
 
 console.dir(scene);
 
-window.addEventListener("resize", onWindowResize, false);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-}
+// window.addEventListener("resize", onWindowResize, false);
+// function onWindowResize() {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer2.setSize(window.innerWidth, window.innerHeight);
+//   render();
+// }
 
 function animate() {
   requestAnimationFrame(animate);
@@ -46,7 +55,10 @@ function animate() {
 }
 
 function render() {
-  renderer.render(scene, camera);
+  renderer1.render(scene, camera1);
+  renderer2.render(scene, camera2);
+  renderer3.render(scene, camera1);
+  renderer4.render(scene, camera1);
 }
 
 animate();
